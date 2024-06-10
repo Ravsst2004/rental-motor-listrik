fa1eb9da1799172dad79ae21f0aeaafaee3ca732
Ravsst2004 <ravindraviswanatha@gmail.com>
2024-06-10 20:06:34 +0800

    Adding 404 page route and style

---
+++ src/layouts/404.php
@@ -0,0 +1,15 @@
+<?php require_once 'header.php' ?>
+<section class="min-h-screen flex items-center justify-center">
+  <div class="bg-white">
+    <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+      <div class="mx-auto max-w-screen-sm text-center">
+        <h1 class="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600">404</h1>
+        <p class="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl">Something's missing.</p>
+        <p class="mb-4 text-lg font-light text-gray-500">Sorry, we can't find that page. You'll find lots to explore on the home page. </p>
+        <a href="/rental-motor-listrik" class="inline-flex text-slate-700 border-2 border-slate-400 bg-primary-600 hover:bg-slate-500 hover:text-white focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center my-4">Back to Homepage</a>
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php require_once 'footer.php' ?>
