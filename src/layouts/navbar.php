9d6a05e7d9c73f72ed0314e92d957874a667bc0b
Ravsst2004 <ravindraviswanatha@gmail.com>
2024-06-13 14:07:41 +0800

    Adding some condition in navbar link text. if on page change color

--- src/layouts/navbar.php
+++ src/layouts/navbar.php
@@ -11,6 +11,8 @@ $role = isset($_SESSION['role']) ? $_SESSION['role'] : null;
 if (isset($_POST['logout'])) {
   $User->logout();
 }
+
+$current_page = basename($_SERVER['PHP_SELF']);
 ?>
 
 <nav class="bg-white border-gray-200 border-b-2">
@@ -31,22 +33,23 @@ if (isset($_POST['logout'])) {
       <ul
         class="flex flex-col font-medium p-4 md:p-0 mt-4 border md:items-center border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white">
         <li>
-          <a href="#" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0"
+          <a href="index.php"
+            class="<?= $current_page == 'index.php' ? 'text-blue-700' : 'text-slate-800' ?> block py-2 px-3 bg-blue-700 rounded md:bg-transparent md:p-0"
             aria-current="page">Home</a>
         </li>
         <?php if ($user): ?>
           <li>
             <a href="service.php"
-              class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Services</a>
+              class="<?= $current_page == 'service.php' ? 'text-blue-700' : 'text-slate-800' ?> block py-2 px-3 bg-blue-700 rounded md:bg-transparent md:p-0">Services</a>
           </li>
         <?php endif; ?>
         <li>
           <a href="#"
-            class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Pricing</a>
+            class="<?= $current_page == 'pricing.php' ? 'text-blue-700' : 'text-slate-800' ?> block py-2 px-3 bg-blue-700 rounded md:bg-transparent md:p-0">Pricing</a>
         </li>
         <li>
           <a href="#"
-            class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Contact</a>
+            class="<?= $current_page == 'contact.php' ? 'text-blue-700' : 'text-slate-800' ?> block py-2 px-3 bg-blue-700 rounded md:bg-transparent md:p-0">Contact</a>
         </li>
         <?php if ($user): ?>
           <li>
